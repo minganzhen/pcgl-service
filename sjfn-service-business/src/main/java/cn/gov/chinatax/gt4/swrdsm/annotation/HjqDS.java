35580d92deabd1c29b0dd9764ffb0e0ca948e498
hax@djf <1435501085@qq.com>
2023-12-05 11:57:23 +0800

    [数据赋能] 数据源调整

--- sjfn-service-business/src/main/java/cn/gov/chinatax/gt4/swrdsm/annotation/HjqDS.java
+++ sjfn-service-business/src/main/java/cn/gov/chinatax/gt4/swrdsm/annotation/HjqDS.java
@@ -18,7 +18,7 @@ import java.lang.annotation.*;
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@DS("hjq")
+@DS("hjqDataSource")
 public @interface HjqDS {
 
 
